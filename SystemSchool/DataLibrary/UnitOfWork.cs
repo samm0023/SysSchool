d72141e6978f103a6cb8628fac0e48378f0cdd5f
samm0023 <ing.samm0023@gmail.com>
2016-05-26 16:18:22 -0400

    Actualizado sistema:

---
+++ SystemSchool/DataLibrary/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemSchool.DataLibrary
+{
+    public class UnitOfWork
+
+    {
+        private SisSchoolContextDataContext context = new SisSchoolContextDataContext();
+        private GenericRepository<Inscripccion> InscripcionRepository;
+        public GenericRepository<Inscripccion> inscripcionRepository
+        {
+            get
+            {
+
+                if (this.inscripcionRepository == null)
+                {
+                    this.inscripcionRepository = new GenericRepository<Inscripccion>(context);
+                }
+                return inscripcionRepository;
+            }
+        }
+
+
+
+
+        public void Save()
+        {
+            context.SubmitChanges();
+           
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+
+
+
+
